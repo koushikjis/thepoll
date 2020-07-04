96e608a2cd371a69a3f498b9ab5896d510e3f3bd
koushikjis <koushikroy.jis@gmail.com>
2020-07-05 00:04:21 +1000

    Refactored poll result module

--- src/components/pollResult/pollResult.js
+++ src/components/pollResult/pollResult.js
@@ -1,34 +1,48 @@
 import React from 'react'
-import { getSinglePollResult } from '../../model/poll/index'
 import {Card, Button, ProgressBar} from 'react-bootstrap'
+import siteConstant from '../../constant.json'
 
 export default function PollResult(props){
-    const id = props.id
-    const pollResultDetails = getSinglePollResult(id)
+    const pollResultDetails = props.pollResultDetails
     const pollResult = pollResultDetails.pollResult
-    let totalVote = 0
-    for (const key in pollResult) {
-        if (pollResult.hasOwnProperty(key)) {
-            totalVote += pollResult[key];
-            
-        }
-    }
+    const totalVote = getTotalVote(pollResult)
+
+    let styles = {
+        margin: '20px',
+        width: '250px',
+        backgroundColor: '#FFDAB9',
+        float: 'left'
+      };
+
     return (
-        <Card>
+        <Card style={styles}>
             <Card.Header as="h5">Featured</Card.Header>
             <Card.Body>
                 <Card.Title>{pollResultDetails.pollTitle}</Card.Title>
                 <Card.Text>Total Votes: {totalVote}</Card.Text>
-                <Card.Text>
-                    
-                        <ProgressBar variant="success" now={pollResult.Arg * (100/totalVote)} />
-                        <ProgressBar variant="info" now={pollResult.Bra * (100/totalVote)} />
-                        <ProgressBar variant="warning" now={pollResult.Ger * (100/totalVote)} />
-                        <ProgressBar variant="danger" now={pollResult.Spa * (100/totalVote)} />
-                    
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                    {
+                        pollResult.map((eachContender, idx)=>{
+                            return(
+                                <div>
+                                    {eachContender.contender}
+                                    <ProgressBar 
+                                        variant={siteConstant.variant[idx]}
+                                        now={(eachContender.votes * 100/totalVote)}
+                                        label={`${eachContender.votes}`} />
+                                </div>
+                            )
+                        })
+                    }                                    
             </Card.Body>
         </Card>
     )
+}
+
+
+function getTotalVote(pollResult) {
+    let totalVote = 0
+    for (let i=0; i < pollResult.length; i++){
+        totalVote += pollResult[i].votes
+    }
+    return totalVote
 }
